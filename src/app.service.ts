7a8b25af7e27711ce94e6ae1aef4a98c7647a378
7coder49 <7coder49@gmail.com>
2026-05-08 21:06:35 +0530

    Initialize NestJS project with essential configurations and structure

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getGreet(): string {
+    return 'Hi Everyone, Welcome to NestJS!';
+  }
+}
